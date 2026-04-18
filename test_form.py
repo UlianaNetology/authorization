cb4437776d0a2aa53badf91b543a5b60d4817545
Ulyanka Ivanova <ulyanka.ivanova.2019@mail.ru>
2026-04-19 01:00:37 +0300

    adding an explicit wait to check for successful registration/receipt of error text

--- test_form.py
+++ test_form.py
@@ -2,6 +2,8 @@ from selenium.webdriver.common.by import By
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import  expected_conditions as EC
 
 
 @pytest.fixture
@@ -28,7 +30,8 @@ def test_successful_login(driver):
 
    submit_button=driver.find_element(By.CLASS_NAME, "radius")
    submit_button.click()
-   success_message = driver.find_element(By.CSS_SELECTOR, "#content > div > h4")
+   success_message = WebDriverWait(driver, 10).until(
+       EC.visibility_of_element_located((By.CSS_SELECTOR, "#content > div > h4")))
    assert "Welcome to the Secure Area. When you are done click logout below." in success_message.text
 
 
@@ -44,5 +47,5 @@ def test_unsuccessful_login(driver):
 
    submit_button=driver.find_element(By.CLASS_NAME, "radius")
    submit_button.click()
-   success_message = driver.find_element(By.CSS_SELECTOR, "#flash")
+   success_message = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#flash")))
    assert "Your username is invalid!" in success_message.text
